1b20a99361e618219e81b90bdcf041ab3e82d275
Daniel Del Rosario Pimienta <danidelrosario7@gmail.com>
2023-06-08 17:56:16 +0000

    Agregar archivos de proyecto.

---
+++ Activation/IActivationHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Timetracker.Activation;
+
+public interface IActivationHandler
+{
+    bool CanHandle(object args);
+
+    Task HandleAsync(object args);
+}
---
+++ Behaviors/NavigationViewHeaderMode.cs
@@ -0,0 +1,8 @@
+﻿namespace Timetracker.Behaviors;
+
+public enum NavigationViewHeaderMode
+{
+    Always,
+    Never,
+    Minimal
+}
